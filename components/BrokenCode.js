25d84dc443b056a551b05a653addeef5c3794c41
Scott Brabson <scottbrabson@gmail.com>
2021-04-19 16:13:20 -0600

    implement getInitialProps w/context

--- components/BrokenCode.js
+++ components/BrokenCode.js
@@ -1,31 +1,8 @@
 import React, { useState } from "react";
-import CoolThings from "./CoolThings";
+import Link from "next/link";
 
 const BrokenCode = ({ name, handleChange }) => {
-  const [show, setShow] = useState(false);
   const [value, setValue] = useState("");
-  const [detailsJSX, setDetailsJSX] = useState(null)
-
-  const fetchInfo = async () => {
-    // 87 total IDs
-    const randomCharID = Math.floor(Math.random() * 88).toFixed(0)
-    const res = await fetch(`https://swapi.py4e.com/api/people/${randomCharID}`)
-    const data = await res.json()
-    return data
-  }
-  
-	const buildJSX = (info) => {
-    if (info.detail) setDetailsJSX(<div>failed to load</div>)
-    if (!info) setDetailsJSX(<div>loading...</div>)
-    console.log(info)
-    setDetailsJSX(<CoolThings character={info}/>)
-  }
-
-  const handleClick = async () => {
-    const info = await fetchInfo()
-    buildJSX(info)
-    setShow(!show)
-  }
 
   return (
     <div>
@@ -45,11 +22,9 @@ const BrokenCode = ({ name, handleChange }) => {
         <div>
           <h1>Hi {name}</h1>
           <br />
-          <button onClick={handleClick}>
-            {show ? "Hide Info" : "Get random character info"}
-          </button>
-          <br />
-          {show && detailsJSX && detailsJSX}
+          <Link href="/character">
+            <button>Get random character info</button>
+          </Link>
         </div>
       )}
     </div>
--- pages/character/index.js
+++ pages/character/index.js
@@ -1,23 +1,36 @@
 import buildClient from "../../api/buildClient";
 
-const CoolThings = ({character: {name, birth_year}}) => {
+const CoolThings = ({ character }) => {
+  const { name, birth_year } = character;
 
-	return (
-		<div>
-			<br/>
-			Character Details
-			<br/>
-			<h3>{name}</h3>
-			<h3>{birth_year}</h3>
-		</div>
-	);
+  if (!character) return <div>loading...</div>;
+  if (character.detail) {
+    console.log("Error, character not found. Please contact website admin");
+    return (
+      <div>
+        Failed to load, please check console for details or <br /> contact the
+        website administrator: scottbrabson@gmail.com
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <br />
+      Character Details
+      <br />
+      <h3>{name}</h3>
+      <h3>{birth_year}</h3>
+    </div>
+  );
 };
 
 CoolThings.getInitialProps = async (context, client) => {
-	const res = await fetch(`https://swapi.py4e.com/api/people/1`);
-  const data = await res.json()
-  console.log('{DATA}',data)
-	return {character: data}
-}
+  // 87 total IDs
+  const randomCharID = Math.floor(Math.random() * 88).toFixed(0);
+  const res = await fetch(`https://swapi.py4e.com/api/people/${randomCharID}`);
+  const data = await res.json();
+  return { character: data };
+};
 
 export default CoolThings;
